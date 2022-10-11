29a9f0074d660938d546246aecdf4540cbc020d3
Sabrina Jodexnis <jodexnis@ubilabs.net>
2022-10-11 16:52:26 +0200

    refactor(map-provider): update type for props

--- src/map-provider.tsx
+++ src/map-provider.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, PropsWithChildren} from 'react';
 
 import GoogleMap from './google-map';
 
@@ -27,8 +27,8 @@ export const GoogleMapContext = React.createContext<GoogleMapContextType>({
 /**
  * The global Google Map provider
  */
-const GoogleMapProvider: React.FunctionComponent<GoogleMapProviderProps> = (
-  props:{children: React.ReactNode} & GoogleMapProviderProps
+const GoogleMapProvider: React.FunctionComponent<PropsWithChildren<GoogleMapProviderProps>> = (
+  props
 ) => {
   const {
     children,
