e1b3a7eeca5ef548b525062773e732339af9f2a5
Sabrina Jodexnis <jodexnis@ubilabs.net>
2022-10-18 16:18:12 +0200

    feat(hooks): add max zoom hook

---
+++ src/hooks/max-zoom.ts
@@ -0,0 +1,25 @@
+import {useMemo} from 'react';
+
+import useGoogleMap from './map-instance';
+
+/**
+ * Hook to get Max Zoom Service instance
+ */
+const useMaxZoomService = (): google.maps.MaxZoomService | null => {
+  const {map} = useGoogleMap();
+
+  // Creates a Max Zoom Service instance
+  const maxZoomService =
+    useMemo<google.maps.MaxZoomService | null>(() => {
+      // Wait for map to be initialized
+      if (!map) {
+        return null;
+      }
+
+      return new google.maps.MaxZoomService();
+    }, [map]);
+
+  return maxZoomService;
+};
+
+export default useMaxZoomService;
