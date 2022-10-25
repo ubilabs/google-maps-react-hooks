d2a4f16dff8d1ede952cb08fd250a531c529131e
Sabrina Jodexnis <jodexnis@ubilabs.net>
2022-10-25 12:06:14 +0200

    feat(examples): add elevation service example

---
+++ examples/elevation/components/elevation/elevation.tsx
@@ -0,0 +1,61 @@
+import {useEffect, useState} from 'react';
+import {
+  useElevationService,
+  useGoogleMap
+} from '@ubilabs/google-maps-react-hooks';
+
+const Elevation = () => {
+  const {map} = useGoogleMap();
+  // Get the elevator from the useElevationService hook
+  const elevator = useElevationService();
+
+  const [infoWindow, setInfoWindow] = useState<google.maps.InfoWindow | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+
+    if (!infoWindow) {
+      // Create a new InfoWindow
+      const initialPosition = {lat: 51.08998021141488, lng: 10.627828045134935};
+      const infowindow = new google.maps.InfoWindow({
+        content: 'Click somewhere on the map to see the elevation',
+        position: initialPosition
+      });
+
+      setInfoWindow(infowindow);
+      map.setCenter(initialPosition);
+
+      return;
+    }
+
+    infoWindow.open(map);
+
+    // Click on the map and open an infowindow with the elevation.
+    map.addListener('click', (mapsMouseEvent: google.maps.MapMouseEvent) => {
+      // Update infowindow with new position and elevation info
+      infoWindow.setPosition(mapsMouseEvent.latLng);
+
+      // Retrieve elevation info from elevator
+      elevator?.getElevationForLocations(
+        {locations: [mapsMouseEvent.latLng]},
+        (results: google.maps.ElevationResult[]) => {
+          // eslint-disable-next-line no-console
+          console.log(results);
+
+          map.setCenter(results[0].location);
+
+          infoWindow.setPosition(results[0].location);
+          infoWindow.setContent(`Elevation: ${results[0].elevation}`);
+        }
+      );
+    });
+  }, [map, infoWindow]);
+
+  return null;
+};
+
+export default Elevation;
