a7c1b07157c22749fbd78e3303dfa7343975de6d
Sabrina Jodexnis <jodexnis@ubilabs.net>
2022-10-27 12:03:30 +0200

    refactor(elevation): use infowindow name for variable

--- examples/elevation/components/elevation/elevation.tsx
+++ examples/elevation/components/elevation/elevation.tsx
@@ -14,24 +14,22 @@ const Elevation = () => {
       return () => {};
     }
 
-    // Create a new InfoWindow
     const initialPosition = {lat: 51.08998021141488, lng: 10.627828045134935};
-    const newInfoWindow = new google.maps.InfoWindow({
+
+    // Create a new InfoWindow
+    const infoWindow = new google.maps.InfoWindow({
       content: 'Click somewhere on the map to see the elevation',
       position: initialPosition
     });
 
     map.setCenter(initialPosition);
 
-    if (!newInfoWindow) {
-      return () => {};
-    }
-    newInfoWindow.open(map);
+    infoWindow.open(map);
 
     // Click on the map and open an infowindow with the elevation.
     map.addListener('click', (mapsMouseEvent: google.maps.MapMouseEvent) => {
       // Update infowindow with new position and elevation info
-      newInfoWindow.setPosition(mapsMouseEvent.latLng);
+      infoWindow.setPosition(mapsMouseEvent.latLng);
 
       // Retrieve elevation info from elevator
       elevator?.getElevationForLocations(
@@ -42,8 +40,8 @@ const Elevation = () => {
 
           map.setCenter(results[0].location);
 
-          newInfoWindow.setPosition(results[0].location);
-          newInfoWindow.setContent(`Elevation: ${results[0].elevation}`);
+          infoWindow.setPosition(results[0].location);
+          infoWindow.setContent(`Elevation: ${results[0].elevation}`);
         }
       );
     });
@@ -52,7 +50,7 @@ const Elevation = () => {
     return () => {
       if (map) {
         google.maps.event.clearListeners(map, 'click');
-        newInfoWindow.close();
+        infoWindow.close();
       }
     };
   }, [map]);
