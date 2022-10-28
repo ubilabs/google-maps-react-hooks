3a468ca563e36f104d707f7bae1681208e31a3f5
Sabrina Jodexnis <jodexnis@ubilabs.net>
2022-10-28 11:54:21 +0200

    refactor(elevation-service): update missing names from elevation to elevation service
    Breaking Changes

--- examples/elevation-service/app.tsx
+++ examples/elevation-service/app.tsx
@@ -2,7 +2,7 @@ import React, {FunctionComponent, useState, useCallback} from 'react';
 import {GoogleMapProvider} from '@ubilabs/google-maps-react-hooks';
 
 import MapCanvas from './components/map-canvas/map-canvas';
-import ElevationService from './components/elevation/elevation-service';
+import ElevationService from './components/elevation-service/elevation-service';
 
 import {GOOGLE_MAPS_API_KEY} from '../constants';
 
