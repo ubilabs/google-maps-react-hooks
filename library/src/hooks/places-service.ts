3068f6c51e17e7896eb921e071b9d0c947e1e142
Sabrina Jodexnis <jodexnis@ubilabs.net>
2022-11-03 15:07:27 +0100

    feat(places-service): add divElement as props to usePlacesService hook

--- library/src/hooks/places-service.ts
+++ library/src/hooks/places-service.ts
@@ -1,28 +1,49 @@
-import {useContext, useMemo} from 'react';
+import {useContext, useEffect, useState} from 'react';
 
 import {GoogleMapsContext} from '../google-maps-provider';
+
+export interface PlacesServiceProps {
+  divElement?: HTMLDivElement | null;
+}
 
 /**
  * Hook to get Google Maps Places Service instance
  */
-export const usePlacesService = (): google.maps.places.PlacesService | null => {
-  const {map} = useContext(GoogleMapsContext);
+export const usePlacesService = (
+  props?: PlacesServiceProps
+): google.maps.places.PlacesService | null => {
+  const {map, googleMapsAPIIsLoaded} = useContext(GoogleMapsContext);
+
+  const [placesService, setPlacesService] =
+    useState<google.maps.places.PlacesService | null>(null);
 
   // Creates a Places Service instance
-  const placesService = useMemo<google.maps.places.PlacesService | null>(() => {
-    // Wait for map to be initialized
-    if (!map) {
-      return null;
-    }
-
-    if (!google.maps.places) {
+  useEffect(() => {
+    if (googleMapsAPIIsLoaded && !google.maps.places) {
       throw Error(
         "Places library missing. Add 'places' to the libraries array of GoogleMapsProvider."
       );
     }
 
-    return new google.maps.places.PlacesService(map);
-  }, [map]);
+    if (!props?.divElement) {
+      // Wait for map to be initialized
+      if (!map) {
+        return;
+      }
+
+      // Create places service and pass map as attrContainer
+      const serviceMap = new google.maps.places.PlacesService(map);
+      setPlacesService(serviceMap);
+
+      return;
+    }
+
+    // Create places service and pass HTMLDivElement as attrContainer
+    const serviceElement = new google.maps.places.PlacesService(
+      props?.divElement
+    );
+    setPlacesService(serviceElement);
+  }, [googleMapsAPIIsLoaded, map, props?.divElement]);
 
   return placesService;
 };
