e2e000baddfd5ca325400a4887236fa311383660
Sabrina Jodexnis <jodexnis@ubilabs.net>
2022-11-03 10:10:10 +0100

    feat(examples): add autocomplete service example

---
+++ examples/places-autocomplete-service/app.tsx
@@ -0,0 +1,45 @@
+import React, {FunctionComponent, useState, useCallback} from 'react';
+import {GoogleMapsProvider} from '@ubilabs/google-maps-react-hooks';
+
+import MapCanvas from './components/map-canvas/map-canvas';
+import PlacesAutocompleteService from './components/places-autocomplete-service/places-autocomplete-service';
+
+import {GOOGLE_MAPS_API_KEY} from '../constants';
+
+import './main.module.css';
+
+const mapOptions = {
+  center: {lat: 53.5582447, lng: 9.647645},
+  zoom: 6,
+  disableDefaultUI: true,
+  zoomControl: false
+};
+
+const App: FunctionComponent<Record<string, unknown>> = () => {
+  const [mapContainer, setMapContainer] = useState<HTMLDivElement | null>(null);
+
+  const mapRef = useCallback(
+    (node: React.SetStateAction<HTMLDivElement | null>) => {
+      node && setMapContainer(node);
+    },
+    []
+  );
+
+  return (
+    <React.StrictMode>
+      <GoogleMapsProvider
+        googleMapsAPIKey={GOOGLE_MAPS_API_KEY}
+        mapContainer={mapContainer}
+        mapOptions={mapOptions}
+        // Add the places library
+        libraries={['places']}>
+        <div id="container">
+          <MapCanvas ref={mapRef} />
+          <PlacesAutocompleteService />
+        </div>
+      </GoogleMapsProvider>
+    </React.StrictMode>
+  );
+};
+
+export default App;
---
+++ examples/places-autocomplete-service/components/places-autocomplete-service/places-autocomplete-service.tsx
@@ -0,0 +1,172 @@
+import React, {
+  FunctionComponent,
+  ChangeEvent,
+  useState,
+  useRef,
+  useEffect
+} from 'react';
+import {
+  useAutocompleteService,
+  useGoogleMap,
+  usePlacesService
+} from '@ubilabs/google-maps-react-hooks';
+import cx from 'classnames';
+
+import styles from './places-autocomplete-service.module.css';
+
+export interface PlacesAutocompleteServiceSuggestion {
+  id: string;
+  label: string;
+}
+
+const PlacesAutocompleteService: FunctionComponent<
+  Record<string, unknown>
+> = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  // eslint-disable-next-line no-undef
+  const timeout = useRef<NodeJS.Timeout | null>(null);
+
+  const [inputValue, setInputValue] = useState<string>('');
+  const [debouncedInputValue, setDebouncedValue] = useState<string>('');
+  const [suggestions, setSuggestions] = useState<
+    Array<PlacesAutocompleteServiceSuggestion>
+  >([]);
+  const [suggestionsAreVisible, setSuggestionsAreVisible] =
+    useState<boolean>(false);
+
+  const map = useGoogleMap();
+  const autocompleteService = useAutocompleteService();
+  const placesService = usePlacesService();
+
+  const maxNumberOfSuggestions = 5;
+
+  // Update the user input value
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+
+    // Update debounced value (for search request) after delay
+    timeout.current = setTimeout(() => {
+      setDebouncedValue(event.target.value);
+
+      // Show dropdown
+      if (!suggestionsAreVisible) {
+        setSuggestionsAreVisible(true);
+      }
+    }, 300);
+  };
+
+  // Handle suggestion selection
+  const selectSuggestion = (
+    suggestion: PlacesAutocompleteServiceSuggestion
+  ) => {
+    inputRef.current?.focus();
+    setInputValue(suggestion.label);
+
+    // Close dropdown
+    setSuggestionsAreVisible(false);
+
+    // Get the location from Places Service of the selected place and zoom to it
+    placesService?.getDetails(
+      {placeId: suggestion.id},
+      (
+        placeResult: google.maps.places.PlaceResult | null,
+        status: google.maps.places.PlacesServiceStatus
+      ) => {
+        if (
+          status !== google.maps.places.PlacesServiceStatus.OK ||
+          !placeResult
+        ) {
+          return;
+        }
+
+        // Get position of the suggestion to move map
+        const position = placeResult.geometry?.location;
+
+        if (map && position) {
+          map.setZoom(14);
+          map.panTo(position);
+        }
+      }
+    );
+  };
+
+  // Update suggestions and get autocomplete place suggestions
+  useEffect(() => {
+    if (inputValue.length >= 2) {
+      autocompleteService?.getPlacePredictions(
+        {
+          input: inputValue
+        },
+        (
+          predictions: google.maps.places.AutocompletePrediction[] | null,
+          status: google.maps.places.PlacesServiceStatus
+        ) => {
+          if (
+            status !== google.maps.places.PlacesServiceStatus.OK ||
+            !predictions
+          ) {
+            return;
+          }
+
+          const autocompleteSuggestions = predictions
+            .slice(0, maxNumberOfSuggestions)
+            .map(prediction => ({
+              id: prediction.place_id,
+              label: prediction.description
+            }));
+
+          // Update suggestions for dropdown suggestions list
+          setSuggestions(autocompleteSuggestions);
+        }
+      );
+    } else {
+      setSuggestions([]);
+    }
+  }, [inputValue]);
+
+  return (
+    <>
+      <input
+        ref={inputRef}
+        className={cx(
+          styles.searchInput,
+          debouncedInputValue.length && styles.expanded
+        )}
+        value={inputValue}
+        onChange={handleInputChange}
+        autoComplete="off"
+        aria-owns="search-suggestions"
+        aria-expanded={debouncedInputValue.length >= 3}
+        aria-autocomplete="list"
+        role="combobox"
+      />
+
+      {suggestionsAreVisible && (
+        <ul
+          className={cx(
+            styles.suggestions,
+            debouncedInputValue.length && styles.expanded
+          )}
+          id="search-suggestions"
+          role="listbox">
+          {suggestions.map((suggestion, index) => (
+            <li
+              key={suggestion.id}
+              onClick={() => selectSuggestion(suggestion)}
+              id={`search-suggestion-${index}`}
+              role="option"
+              tabIndex={-1}>
+              <span>{suggestion.label}</span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default PlacesAutocompleteService;
