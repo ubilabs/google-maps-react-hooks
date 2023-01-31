9f9634b6a9cac3f63e7bcc3e150654af8d3758a9
Martin Schuhfuss <m.schuhfuss@gmail.com>
2023-01-31 12:30:34 +0100

    feat(core): refactor provider implementation to use new loader (squash later)

--- library/src/google-maps-api-loader.ts
+++ library/src/google-maps-api-loader.ts
@@ -1,12 +1,12 @@
 const MAPS_API_BASE_URL = 'https://maps.googleapis.com/maps/api/js';
 
 export type ApiParams = {
-  v: string;
   key: string;
-  language: string;
-  region: string;
-  libraries: string;
-  auth_referrer_policy: string;
+  v?: string;
+  language?: string;
+  region?: string;
+  libraries?: string;
+  authReferrerPolicy?: string;
 };
 
 const enum LoadingState {
@@ -44,12 +44,13 @@ export class GoogleMapsApiLoader {
    */
   static async load(params: ApiParams): Promise<void> {
     const serializedParams = this.serializeParams(params);
-
+    console.debug('api-loader: load', params);
     this.params = params;
 
     // loading hasn't yet started, so the promise can be reused (loading will
     // use parameters from the last call before loading starts)
     if (this.loadingState <= LoadingState.QUEUED && this.loadPromise) {
+      console.debug('api-loader: loading is already queued');
       return this.loadPromise;
     }
 
@@ -59,44 +60,70 @@ export class GoogleMapsApiLoader {
       serializedParams === this.serializedParams &&
       this.loadPromise
     ) {
+      console.debug('api-loader: loading already started');
       return this.loadPromise;
     }
 
     // if parameters did change, and we already loaded the API, we need
     // to unload it first.
     if (this.loadPromise) {
+      // FIXME: in this case, we might want to report an error if we're not
+      //  already unloading, since that would only be the case if the loader
+      //  is taked with loading multiple instances of the API with different
+      //  parameters.
+
+      // if (this.loadingState >= LoadingState.LOADING) {
+      //   console.error(
+      //     'The Google Maps API Parameters passed to the `GoogleMapsProvider` ' +
+      //       'components do not match. The Google Maps API can only be loaded ' +
+      //       'once. Please make sure to pass the same API parameters to all ' +
+      //       'of your `GoogleMapsProvider` components.'
+      //   );
+      // }
+      console.debug(
+        'api-loader: was already loaded with other params, unload first'
+      );
       await this.unloadAsync();
     }
 
+    console.debug('api-loader: queue request');
+
     this.loadingState = LoadingState.QUEUED;
     this.loadPromise = new Promise(async (resolve, reject) => {
+      console.debug('api-loader: defer to microtasks');
       // this causes the rest of the function to be pushed back into the
       // microtask-queue, allowing multiple synchronous calls before actually
       // loading anything.
       await Promise.resolve();
+      console.debug('api-loader: continue');
 
       // if the load request was canceled in the meantime, we stop here and
       // reject the promise. This is typically the case in react dev mode when
       // load/unload are called from a hook.
       if (this.loadingState !== LoadingState.QUEUED) {
+        console.debug('api-loader: no longer queued');
         reject(new Error('map loading canceled'));
         return;
       }
 
+      console.debug('api-loader: start actually loading');
       this.loadingState = LoadingState.LOADING;
       const url = this.getApiUrl(this.params);
 
       // setup the callback
       window.__gmcb__ = () => {
+        console.debug(`api-loader: callback called, loading complete`);
         this.loadingState = LoadingState.LOADED;
         resolve();
       };
       url.searchParams.set('callback', '__gmcb__');
+      console.debug(`api-loader: URL: ${url}`);
 
       // create the script
       const script = document.createElement('script');
       script.src = url.toString();
       script.onerror = err => reject(err);
+
       document.head.appendChild(script);
     });
 
@@ -155,7 +182,15 @@ export class GoogleMapsApiLoader {
 
     const url = new URL(MAPS_API_BASE_URL);
     for (const [key, value] of Object.entries(params)) {
-      url.searchParams.set(key, value);
+      if (value === undefined) {
+        continue;
+      }
+
+      url.searchParams.set(
+        // camelCase to snake_case conversion
+        key.replace(/[A-Z]/g, c => `_${c.toLowerCase()}`),
+        value
+      );
     }
 
     return url;
@@ -168,7 +203,7 @@ export class GoogleMapsApiLoader {
       params.language,
       params.region,
       params.libraries,
-      params.auth_referrer_policy
+      params.authReferrerPolicy
     ].join('/');
   }
 }
